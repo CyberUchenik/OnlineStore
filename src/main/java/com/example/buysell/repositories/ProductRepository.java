3e6bd7173d733de1de1b6064df02da9b2a87b644
Azamat Koshenov <CCAzamat Koshenov>
2024-04-10 17:44:42 +0600

    Changes from Aza

--- src/main/java/com/example/buysell/repositories/ProductRepository.java
+++ src/main/java/com/example/buysell/repositories/ProductRepository.java
@@ -5,19 +5,10 @@
 
     import java.util.List;
 
-<<<<<<< HEAD
-    public interface ProductRepository extends JpaRepository<Product, Long> {
-        List<Product> findByTitle(String title);
-        List<Product> findByCity(String city);
-        List<Product> findByTitleAndCity(String title, String city);
-
-=======
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByTitle(String title);
     List<Product> findByCity(String city);
     List<Product> findByTitleAndCity(String title, String city);
     List<Product> findByCatalog(String catalog);
     List<Product> findByCityAndCatalog(String city, String catalog);
->>>>>>> a91969f967a174362f9e966d398285aa6a2dedf0
-
     }
